84872823eb67e111b6b0c5a9ce6b0102185cb84b
Tyler <tjcowman@gmail.com>
2020-07-23 19:43:59 -0400

    ...

--- server/validation/bulkQuery.py
+++ server/validation/bulkQuery.py
@@ -1,40 +1,132 @@
 #! /usr/bin/python3
-
+import math
 import requests
 import json
 
 import numpy as np
 from functools import reduce
+from statistics import mean
 
 hostname = 'http://localhost:9060/'
 
+def bitExtract(n) :
+    ids = []
+    counter = 0
+    while n > 0:
+        if n % 2 > 0:
+            ids.append(counter)
+        counter+=1
+        n = n >> 1
+    return ids
 
-#TODO Make a serverState object
+
+
 class ServerState:
     def __init__(self, hostname):
         self.hostname = hostname
-        self.nodeData = []
+        self.nodeData = []  
         self.nodeNameMap = dict()
         
         self.updateFromManifest()
         
     def updateFromManifest(self):
         rls =requests.get(hostname+'ls').json()
+        self.nodeData = [None] * rls['nodes']
         self.versionClasses = determineVersionClass(rls)
         self.edgeLabelBitMaps = getEdgeLabelBitMaps(rls)
         self.nodeLabelBitMaps = getNodeLabelBitMaps(rls)
         
     def lookupNames(self, nameList):
-        nameList = list(filter(lambda x : x not in self.nodeNameMap,nameList))
-        rq = requests.post(hostname, data=json.dumps({'cmd':'lkpn', 'names':nameList}))
+        nameListQ = list(filter(lambda x : x not in self.nodeNameMap,nameList))
         
-        print(rq.json())
-        #update the newly acquired elements
-        for i, e in enumerate(rq.json()):
-            #print(i, e)
-            self.nodeNameMap[nameList[i]] = e['id']
+        if len(nameListQ) > 0:
+            rq = requests.post(hostname, data=json.dumps({'cmd':'lkpn', 'names':nameListQ}))
+        
+            #update the newly acquired elements
+            for i, e in enumerate(rq.json()):
+                self.nodeNameMap[nameListQ[i]] = e['id']
+                if e['id'] != -1:
+                    self.nodeData[e['id']] = {'name' : nameListQ[i], 'label': e['l'] }
+        
+        return map(lambda x: self.nodeNameMap[x],nameList)
 
+    #Like regular lookup, except looks up an converts the specified input column
+    def lookupNameCol(self, matrix, nameCol):
+        #print(matrix)
+        ids = list(self.lookupNames(list((map(lambda x : x[nameCol], matrix)))))
+        for i,row in enumerate(matrix):
+            #print(i, row)
+            row[nameCol] = ids[i]
+            
+        return matrix
+        #print(names)
 
+    #Gets the full version definition for a set of version indexes (Selects all node and edge labe;s)
+    def versionDef(self,versions):
+        nodeLabelBits = map(lambda x: self.nodeLabelBitMaps[x] ,versions)
+        nodeLabelBits = reduce(lambda x1,x2 : x1|x2 ,nodeLabelBits)
+    
+        edgeLabelBits = map(lambda x: self.edgeLabelBitMaps[x] ,versions)
+        edgeLabelBits = reduce(lambda x1,x2 : x1|x2 ,edgeLabelBits)
+        
+        #return {'versions' : versions, 'vertexLabels' : bitExtract(nodeLabelBits), 'edgeLabels' : bitExtract(edgeLabelBits)}
+        return {'versions' : [versions], 'vertexLabels' : [bitExtract(nodeLabelBits)], 'edgeLabels' : [bitExtract(edgeLabelBits)]}
+    
+class Path:
+    def __init__(self, serverResponse):
+        self.nodeScore=serverResponse['nodeScore']
+        self.direction=serverResponse['direction']
+        self.nodes= serverResponse['nodes']
+        self.edgeLabels= serverResponse['edgeLabels']
+        self.totalWeight = serverResponse['totalWeight']
+        
+        #Get the nodes and eges used
+        self.nodeSet = set(self.nodes)
+        self.edgeSet = set()
+        for i in range(0,len(self.nodes)-1):
+            self.edgeSet.add(str(self.nodes[i]) + str(self.nodes[i+1]))
+        
+        
+        
+    def __repr__(self):
+        return str(self.nodeScore) + '\n' + str(self.nodes)
+        
+class PathTree:
+    def __init__(self, serverResponse ):
+        self.paths = []
+        
+        for path in serverResponse:
+            self.paths.append(Path(path))
+            
+        self.nodeSet = reduce(lambda x1,x2 : x1.union(x2), map( lambda x: x.nodeSet, self.paths))
+        self.edgeSet = reduce(lambda x1,x2 : x1.union(x2), map( lambda x: x.edgeSet, self.paths))
+        
+        
+        #print(self.nodeSet)
+     
+    def __repr__(self):
+        return '\n'.join(map(str,self.paths))
+        
+class PathForest:
+    def __init__(self, serverResponse):
+        self.trees = []
+        
+        for tree in serverResponse['trees']:
+            self.trees.append(PathTree(tree))
+            
+            
+    def __repr__(self):
+        return '\n'.join(map(str, self.trees))
+        
+    def intersection(self):
+        return [reduce(lambda x1, x2: x1.intersection(x2), map(lambda x: x.nodeSet, self.trees)),
+                reduce(lambda x1, x2: x1.intersection(x2), map(lambda x: x.edgeSet, self.trees))]
+    
+    def meanSize(self):
+        return [mean(map(lambda x: len(x.nodeSet), self.trees)),
+                mean(map(lambda x: len(x.edgeSet), self.trees))]
+        
+        
 def determineVersionClass(rls):
     vs = {'PPI' : set(), 'PHS' : set(),  'KS' : set(), 'COC' : set()}
     for v in rls['versions']:
@@ -51,69 +143,153 @@ def getNodeLabelBitMaps(rls):
     return rls['labelsUsed']['nodes']
 
 
-def bitExtract(n) :
-    
-    #print(n)
-    ids = []
-    counter = 0
-    while n > 0:
-        if n % 2 > 0:
-            ids.append(counter)
-        counter+=1
-        n = n >> 1
-    return ids
-
-#Takes the list of version ids and returns them with the corresponding lists of labelIds 
-def selectAllLabels(versionIds, nodeLabelBitMaps, edgeLabelBitMaps):
-
-    
-    nodeLabelBits = map(lambda x: nodeLabelBitMaps[x] ,versionIds)# versionIds.map(nodeLabelBitMaps,index)
-    nodeLabelBits = reduce(lambda x1,x2 : x1|x2 ,nodeLabelBits)
-    #print(nodeLabelBits)
-    
-    edgeLabelBits = map(lambda x: edgeLabelBitMaps[x] ,versionIds)
-    edgeLabelBits = reduce(lambda x1,x2 : x1|x2 ,edgeLabelBits)
-    
-    return {'versions' : versionIds, 'nodeLabels' : bitExtract(nodeLabelBits), 'edgeLabels' : bitExtract(edgeLabelBits)}
-    
+def parseSites(filename):
+    sites = []
+    with open(filename, 'r') as f:
+        for line in f:
+            tokens = line.strip('\n').split('\t')
+            
+            for i in range(1,len(tokens)):
+                tokens[i] = float(tokens[i])
+                
+            #print(tokens)
+            if tokens is not None:
+                sites.append(tokens)
+        
+        return sites
  
-#def lookupNames(nameList):
-    #nameList = nameList.select(lambda x : )
-    #rq = requests.post(hostname, 'cmd':'lkpn', 'names':nameList)
-                       
+#def runQuerySet(ss, ):
+
+class QuerySet():
+    def __init__(self):
+        self.endFlag = True
+        self.iteration = 0
+        self.lsb = 0
+        self.args = []
+        self.trackArgVals = []
+
+    def addArg(self, argPair, trackArgVal=False):
+        self.args.append(argPair)
+        self.trackArgVals.append(trackArgVal)
+
+        
+    def addArgs(self, argPairs, trackArgVal=False):
+        for p in argPairs.items():
+            self.args.append(p)
+            self.trackArgVals.append(trackArgVal)
+        
+    def begin(self):
+        #print(self.args)
+        
+        
+        #self.iteration = 0
+        self.lsb = 0
+        self.bases = list(map(lambda x: len(x[1]), self.args))
+        self.bases.append(2) #The end flag needs 2 states 
+        self.baseCounter = [0] *len(self.bases)
+        self.endFlag = False;
+        #print(list(self.bases))
+
+    def end(self):
+        return self.endFlag
+
+    def baseIncrement(self):
+        #print(self.bases)
+       
+        if not self.end():
+            self.baseCounter[self.lsb]+=1
+
+            #rollover
+            while self.baseCounter[self.lsb] > (self.bases[self.lsb]-1):
+                self.baseCounter[self.lsb] = 0   
+                self.lsb+=1
+                self.baseCounter[self.lsb] +=1
+                
+            self.lsb=0
+            
+            if self.baseCounter[len(self.baseCounter)-1] ==1:
+                self.endFlag = True
+            
+        #self.iteration += 1
+        #print(self.baseCounter)
+       
+    def getArgs(self):
+        m = {}
+
+        for i in range(0, len(self.args)):
+            m[self.args[i][0]] = self.args[i][1][self.baseCounter[i]]
+        #return map(map(lambda x: {self.args[x][0] : self.args[x][1][self.] },self.baseCounter))
+        return m
+        
+    def iterateAll(self):
+        self.begin()
+        
+        while not self.end():
+            print(self.getArgs())
+            self.baseIncrement()
+            
+            
+    def getTrackedArgs(self):
+        m = []
+        for i in range(0, len(self.args)):
+            if self.trackArgVals[i]:
+                m.append(str(self.args[i][1][self.baseCounter[i]]))
+        
+        return '\t'.join(m)
+        
+    def getTrackedArgsHeader(self):
+        m = []
+        for i in range(0, len(self.args)):
+            if self.trackArgVals[i]:
+                m.append(str(self.args[i][0]))
+        
+        return '\t'.join(m)
+    #def nextPayload():
+        #return 
  
 def main():
     ss = ServerState(hostname)
-    ss.lookupNames(['Q15459', 'ff'])
-    print(ss.nodeNameMap)
-    #print(ss.hostna)
+    q = QuerySet()
     
-    #print('starting')
-    #rls =requests.get(hostname+'ls').json()
-    ##print(rls)
-    
-    #nodeData = []
-    #nodeNameMap = {}
-    
-    #versionClasses = determineVersionClass(rls)
-    
-    #edgeLabelBitMaps = getEdgeLabelBitMaps(rls)
-    #nodeLabelBitMaps = getNodeLabelBitMaps(rls)
-    
-    #versionDef = selectAllLabels([1,21,22,20], nodeLabelBitMaps, edgeLabelBitMaps)
-    
-    #sites = [['Q15459',359,-1.3219], ['Q15459',451,0.5352], ['P28482',185,4.4463], ['P28482',187,4.4195], ['Q8N3F8',273,-0.3219]]
+    versionDef = ss.versionDef([1,2,20,21,22])
+
+    sites =  parseSites('/home/tcowman/githubProjects/cophos/data/raw/Processed_perturb/Phosphorylation_Data/phospho_data_c14.csv.uniprot')  #[['Q15459',359,-1.3219], ['Q15459',451,0.5352], ['P28482',185,4.4463], ['P28482',187,4.4195], ['Q8N3F8',273,-0.3219]]
     #kinase = ['P00533','P15056']
-    #weightFraction = 1
-    #lookupType = 'uniprot'
-    #mechRatio = 100
-    #localProximity = False
+    
+    q.addArg(['cmd', ['pths']])
+    q.addArgs(versionDef)
+    q.addArg(['kinase' , [['P00533','P15056']]])
+    q.addArg(['sites', [sites]])
+    
+    q.addArg(['weightFraction', [1, .5, .2, .1, .05]], True)
+    
+    q.addArg(['lookupType', ['uniprot']])
+    
+    q.addArg(['mechRatio', [100]], True)
+    
+    q.addArg(['localProximity', [False]])
+    
+    
+    #q.iterateAll()
+    
+    q.begin()
+    print(q.getTrackedArgsHeader())
+    while not q.end():
+        pload = q.getArgs()
+        rq = requests.post(hostname, data=json.dumps(pload))
+        
+        #analysis code
+        F = PathForest(rq.json())
+        
+       
+        print(q.getTrackedArgs(), 
+              list(map(lambda x: len(x), F.intersection())), 
+              F.meanSize())
+        
+        q.baseIncrement()
     
  
-    
-    #pload = {'cmd' : 'pths', **versionDef, 'kinase':kinase, 'sites':sites, 'weightFraction':weightFraction, 'lookupType':lookupType, 'mechRatio':mechRatio, 'localProximity':localProximity }
-    #rq = requests.post(hostname, data=json.dumps(pload))
-    #print(rq.json());
+  
 
 if __name__ == "__main__":
     main()
